08835a20d0fcdd5c20ad932d6877893bec771e8c
skhrumalo <skhrumalo@rusteletech.ru>
2022-05-22 06:42:09 +0000

    Update dwc3-baikal.c

--- drivers/usb/dwc3/dwc3-baikal.c
+++ drivers/usb/dwc3/dwc3-baikal.c
@@ -147,8 +147,8 @@ static int dwc3_be_of_register_core /*dwc3_*/ (struct platform_device *pdev) {
 		printk (KERN_INFO "%s: failed registering dwc3 core , error - %d \n",__func__,ret);
 		return  ret;
 	}
-	be->dwc3 =
-		of_find_device_by_node (dwc3_np) ;
+	be->dwc3 = of_find_device_by_node (dwc3_np) ;
+
 	if(!be->dwc3) {
 		printk (KERN_INFO "%s: cant get dwc3 platform device \n",__func__);
 		return -ENODEV ;
@@ -203,7 +203,7 @@ be_dwc3_probe(struct platform_device *pdev)
 	struct dwc3_baikal	*dwc;
 	struct resource         *res, *parent_res = NULL ;
 	int			ret;
-	int    numbe ;
+	int    numbe;
 
 	dwc = devm_kzalloc(dev, sizeof(*dwc), GFP_KERNEL);
 	if (!dwc)
@@ -215,12 +215,7 @@ be_dwc3_probe(struct platform_device *pdev)
 		return ret;
 	}
 	platform_set_drvdata(pdev, dwc);
-	dwc->dev = &pdev->dev ;
-
-
-        //dwc3_be_setbits
-	be_get_ctrl_regs (dwc->qscratch_base,0x0);         //  be->qscratch_base
-
+	dwc->dev = &pdev->dev;
 
 	/***
 	dwc->clk = devm_clk_get(dwc->dev, "usb");
@@ -233,17 +228,17 @@ be_dwc3_probe(struct platform_device *pdev)
 		printk (KERN_INFO  "%s  unable to enable usb clock \n",__func__) ;
 		return ret;
 	}     ***/
+
 	numbe = of_clk_get_parent_count (node);
 	if (numbe >= 0)  {
-	ret = dwc3_be_clk_init (dwc, /*of_clk_get_parent_count(node)*/numbe);
-	if (ret) {
+	  ret = dwc3_be_clk_init (dwc,numbe);
+	  if (ret) {
 		printk (KERN_INFO "%s     cant init BE clk's   \n",__func__);
 		return  ret;
-	}
+	  }
 	} else {
             printk (KERN_INFO "%s   incorrect number of clk's \n", __func__) ;
 	}
-
 	/*********
 	ret = dwc3_qcom_clk_init(qcom, of_clk_get_parent_count(np));
 	if (ret) {
@@ -251,39 +246,39 @@ be_dwc3_probe(struct platform_device *pdev)
 		goto reset_assert;
 	}  *******/
 
-
-
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (node) {
 		parent_res = res;
-	} else {
-		parent_res = kmemdup(res, sizeof(struct resource), GFP_KERNEL);
-		if (!parent_res)
-			return -ENOMEM;
-		parent_res->start = res->start + /*qcom*/dwc->acpi_pdata->qscratch_base_offset;
-		parent_res->end = parent_res->start + /*qcom*/dwc->acpi_pdata->qscratch_base_size;
-	}
+	} 
+	else
+		return -1;
 
+	dwc->qscratch_base = res->start ;
+	printk (
+	KERN_INFO "%s  dwc->qscratch_base from - platform_get_resource:  0x%16x \n",
+	__func__,dwc->qscratch_base)  ;
+
+	
 	if (node) {
-		//ret = of_platform_populate(node, NULL, NULL, dev);
 		ret = dwc3_be_of_register_core (pdev) ;  // should be implement
 		if (ret) {
 			dev_err(&pdev->dev, "failed to register dwc3 core\n");
 			goto __error;
 		}
-	} else {
-		dev_err(dev, "no device node, failed to add dwc3 core\n");
-		ret = -ENODEV;
-		goto __error;
 	}
 
+
+	dwc->qscratch_base =  USB3_BASE_ADDRESS ;
+	// what is dwc->qscratch_base ???
+        //dwc3_be_setbits
+	be_get_ctrl_regs (dwc->qscratch_base, 0x0);         //  be->qscratch_base
+
+
 	dwc->mode = usb_get_dr_mode (&dwc->dwc3->dev) ;
-
-	if (dwc->mode == USB_DR_MODE_PERIPHERAL)
-		dwc3_be_vbus_override_enable(dwc, true);
-
+	if (dwc->mode == USB_DR_MODE_PERIPHERAL)  {
+		//dwc3_be_vbus_override_enable(dwc, true);
+	}
 	return 0;
-
 __error:
 	//clk_disable_unprepare(dwc->clk);
 	return ret;
@@ -308,7 +303,6 @@ static int be_dwc3_remove(struct platform_device *pdev)
 		clk_disable_unprepare(dwc->clks[i]);
 		clk_put(dwc->clks[i]);
 	}
-
 	platform_set_drvdata(pdev, NULL);
 
 	return 0;
@@ -329,9 +323,7 @@ static struct platform_driver be_dwc3_driver = {
 		.of_match_table	= be_dwc3_of_match,
 	},
 };
-
 module_platform_driver(be_dwc3_driver);
-
 MODULE_ALIAS("platform:baikal-dwc3");
 MODULE_AUTHOR("Dmitry Dunaev <dmitry.dunaev@baikalelectronics.ru>");
 MODULE_LICENSE("GPL v2");
